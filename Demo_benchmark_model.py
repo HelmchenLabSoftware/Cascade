af72ae391955fb35d597010df6e6ead1d810b26b
PTRRupprecht <peter.rupprecht@fmi.ch>
2020-06-14 23:40:24 +0200

    Demo script to quantify out-of-dataset generalization for a given model
    Former-commit-id: f084b5f62352ec62937a463ce061f0ffdbb07cb8 [formerly f084b5f62352ec62937a463ce061f0ffdbb07cb8 [formerly f084b5f62352ec62937a463ce061f0ffdbb07cb8 [formerly b135f26f598d30be4db7db015976c525c07d1ef1]]]
    Former-commit-id: 37ff5fa5257012039f114a17265d18139cc8ec3e
    Former-commit-id: 58d1e569224d2fe3a970372151eee1e6cf635456
    Former-commit-id: 86d0bcb5ee1e14b95756a54378abe79eddcf8a33

---
+++ Demo_benchmark_model.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Demo script to quantify out-of-dataset generalizaton for an already trained model:
+  
+Documentation: to be improved
+  
+"""
+
+
+
+"""
+
+Import python packages
+
+"""
+
+import os
+import shutil
+
+if 'Notebooks' in os.getcwd(): os.chdir('..')  # change to main directory
+print('Current directory: {}'.format( os.getcwd() ))
+
+import keras
+from copy import deepcopy
+from scipy.ndimage.filters import gaussian_filter
+import numpy as np
+
+# perform checks to catch most likly import errors
+from cascade2p import checks    # TODO: put all of this in one function
+print('\nChecks for packages:')
+checks.check_packages()
+
+from cascade2p import cascade
+from cascade2p import config
+from cascade2p import utils
+
+"""
+
+Check which model to benchmark, and at which noise level
+
+"""
+
+model_name =  'Universal_30Hz'
+noise_level = 2
+
+
+"""
+
+Load model and parameters
+
+"""
+
+
+model_path = os.path.join('Pretrained_models', model_name)
+cfg_file = os.path.join( model_path, 'config.yaml')
+
+# check if configuration file can be found
+if not os.path.isfile(cfg_file):
+    m = 'The configuration file "config.yaml" can not be found at the location "{}".\n'.format( os.path.abspath(cfg_file) ) + \
+        'You have provided the model "{}" at the absolute or relative path "{}".\n'.format( model_name, model_folder) + \
+        'Please check if there is a folder for model "{}" at the location "{}".'.format( model_name, os.path.abspath(model_folder))
+    print(m)
+    raise Exception(m)
+
+# Load config file
+cfg = config.read_config( cfg_file )
+
+all_training_datasets = cfg['training_datasets']
+
+
+
+"""
+
+Train a model with all datasets and then test with the remaining dataset
+For each model, correlation, error and bias will be computed
+The distribution of these values is the estimate for performance on unseen datasets
+
+"""
+
+
+correlation = []
+error = []
+bias = []
+
+for index in len(all_training_datasets):
+
+  training_datasets = deepcopy(all_training_datasets)
+  test_dataset = training_datasets.pop(index);
+  
+  cfg['training_datasets'] = training_datasets
+  cfg['model_name'] = 'temporary model'
+  cfg['noise_levels'] = [noise_level]
+  
+  
+  cascade.create_model_folder( cfg )
+  
+  cascade.train_model( cfg['model_name'])
+  
+  
+  
+  # extract values from config file into variables
+  test_dataset = [os.path.join('Ground_truth', ds) for ds in [test_dataset]]
+  
+  # test model with the one remaining test_dataset
+  calcium, ground_truth = utils.preprocess_groundtruth_artificial_noise_balanced(
+                              ground_truth_folders = test_dataset,
+                              before_frac = cfg['before_frac'],
+                              windowsize = cfg['windowsize'],
+                              after_frac = 1 - cfg['before_frac'],
+                              noise_level = noise_level,
+                              sampling_rate = cfg['sampling_rate'],
+                              smoothing = cfg['smoothing'] * cfg['sampling_rate'],
+                              omission_list = [],
+                              permute = 0,
+                              verbose = cfg['verbose'],
+                              replicas = 0)
+  calcium = calcium[:,32,]
+  ground_truth = ground_truth[:,]
+  
+  # perform predictions
+  spike_rates = cascade.predict( model_name, calcium.T )
+  spike_rates = np.squeeze(spike_rates)
+  
+  # take only non-nan values
+  nnan_ix = ~np.isnan(spike_rates)
+  ground_truth = ground_truth[nnan_ix]
+  spike_rates = spike_rates[nnan_ix]
+
+  # compute performance metrics
+  ground_truth_smooth = gaussian_filter(ground_truth.astype(float), sigma=cfg['smoothing'] * cfg['sampling_rate'])
+  spike_rates_smooth = gaussian_filter(spike_rates.astype(float), sigma=cfg['smoothing'] * cfg['sampling_rate'])
+
+  error_diff = (spike_rates_smooth - ground_truth_smooth.T)
+  error_pos = np.sum(error_diff[error_diff>0])
+  error_neg = np.sum(error_diff[error_diff<0])
+  error_total = np.sum(np.abs(error_diff))
+  signal = np.sum(ground_truth_smooth)
+
+  error.append(error_total/signal)
+  bias.append((error_pos+error_neg)/signal)
+  correlation.append(np.corrcoef(ground_truth,spike_rates,rowvar=False)[0,1]) 
+
+  # delete temporary model from disk
+  model_path = os.path.join('Pretrained_models', cfg['model_name'])
+  shutil.rmtree(model_path)
+"""
+
+Results
+
+- correlation with ground truth, across different datasets
+- error
+- bias
+
+"""
+
+m_correlation,std_correlation = np.nanmedian(correlation),np.nanstd(correlation)
+m_error,std_error = np.nanmedian(error),np.nanstd(error)
+m_bias,stdbias = np.nanmedian(bias),np.nanstd(bias)
+
+
