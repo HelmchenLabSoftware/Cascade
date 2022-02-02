3f245975317f53944ced9121a42e0301641b639f
Peter Rupprecht <PTRRupprecht@users.noreply.github.com>
2022-02-02 19:51:44 +0100

    Fix potential incompatibility issue

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(
     author="Peter Rupprecht",
     author_email="",
     packages=find_packages(),
-    python_requires=">=3.6, <3.8",
+    python_requires=">=3.7, <3.8",
     install_requires=[
         "numpy",
         "scipy",
         "matplotlib",
         "tensorflow==2.3",  # pip install CPU and GPU tensorflow
         "keras==2.3.1",
-        "h5py==2.10.0",
+        "h5py",
         "seaborn",
         "ruamel.yaml",
         "spyder",
